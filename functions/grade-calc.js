f82900066df2e2dc8b6c965db166d3141bb3f5d7
petros <pit.trak@gmail.com>
2020-06-06 15:08:50 +0300

    grade calculator

---
+++ functions/grade-calc.js
@@ -0,0 +1,25 @@
+// student score - total possible score
+// 15/20 -> You got a C  (75%)
+// A 90->100, B 80->89, C 70->79, D 60->69, F 0->59
+
+
+
+function testResult(studentScore, totalPossibleScore = 20) {
+  let percentage = (studentScore / totalPossibleScore) * 100;
+  let letterGrade = '';
+  if (percentage >= 90) {
+    letterGrade = 'A';
+  }else if (percentage >=89) {
+    letterGrade = 'B';
+  }else if (percentage >= 79) {
+    letterGrade = 'C';
+  }else if (percentage >= 69) {
+    letterGrade = 'D';
+  }else
+    letterGrade = 'F';
+
+  return `You got an ${letterGrade} (${percentage}%)`
+}
+
+let result = testResult(14);
+console.log(result);
