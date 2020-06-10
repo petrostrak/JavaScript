e1201df1d01c0275b8319facf8c0f62b6c4a8e7d
petros <pit.trak@gmail.com>
2020-06-10 15:26:39 +0300

    practice on saving data in local storage

--- todo/js-file.js
+++ todo/js-file.js
@@ -1,25 +1,15 @@
-const todos = [{
-    text: 'Order cat food',
-    completed: false
-  }, {
-    text: 'Clean kitchen',
-    completed: true
-  }, {
-    text: 'Buy food',
-    completed: true
-  }, {
-    text: 'Do work',
-    completed: false
-  }, {
-    text: 'Exercise',
-    completed: true
-  }]
+let todos = []
   
   const filters = {
     searchText: '',
     hideCompleted: false
   }
   
+  const todosJSON = localStorage.getItem('todos')
+  if(todosJSON !== null){
+    todos = JSON.parse(todosJSON)
+  }
+
   const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function (todo) {
         const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
@@ -58,6 +48,7 @@ const todos = [{
         text: e.target.elements.text.value,
         completed: false
     })
+    localStorage.setItem('todos', JSON.stringify(todos))
     renderTodos(todos, filters)
     e.target.elements.text.value = ''
   })
